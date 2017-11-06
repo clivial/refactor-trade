adbd389278ec07be8e2f842c1d5f580b726fd212
刘君 <liujun@xianjinxia.com>
2017-11-06 20:23:35 +0800

    commit project

---
+++ src/main/java/com/clivial/RefactorTradeApplication.java
@@ -0,0 +1,12 @@
+package com.clivial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefactorTradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefactorTradeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/clivial/RefactorTradeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.clivial;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RefactorTradeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
